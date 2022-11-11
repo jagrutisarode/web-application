c612049b9f3577cba48a3805af857e3612e23d9d
jagrutisarode <117511728+jagrutisarode@users.noreply.github.com>
2022-11-11 18:17:34 +0530

    Add files via upload

---
+++ NodeTimer/timer1.js
@@ -0,0 +1,19 @@
+
+//callback function
+//even handler
+
+var sayHello=function (){
+    console.log("Welcome to node js");
+};
+
+
+var sayBye=function (){
+    console.log("Bye Bye........");
+};
+
+//sayHello(); //explicit calling
+
+//register function
+
+setInterval(sayHello,3000);
+setInterval(sayBye, 5000);
